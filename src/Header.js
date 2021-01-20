0f90525e0424fc600edaa8f6882e017893df5f4d
mike <apolloproductionsdj@gmail.com>
2021-01-20 11:28:24 -0700

    created <Header /> and styled it

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './Header.css';
 import MenuIcon from '@material-ui/icons/Menu';
-import { IconButton } from "@material-ui/core";
+import { Icon, IconButton } from "@material-ui/core";
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Avatar from '@material-ui/core/Avatar';
+
 
 function Header() {
     return (
@@ -17,11 +23,25 @@ function Header() {
             </div>
 
             <div className="header__middle">
-
+                <SearchIcon />
+                <input
+                    placeholder="Search mail"
+                    type="text"
+                />
+                <ArrowDropDownIcon className="header__inputCaret" />
             </div>
 
             <div className="header__right">
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsIcon />
+                </IconButton>
+                {/* <IconButton> */}
+                <Avatar />
 
+                {/* </IconButton> */}
             </div>
         </div>
     )
